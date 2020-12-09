5319cd2e8e4d55e0fd27d3f83d0588b47d70dfc4
renatoAraujoSantos <50000041+renatoAraujoSantos@users.noreply.github.com>
2020-12-09 04:01:14 -0300

    fontes

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Profile from './src/Profile';
+import useCachedResources from './hooks/useCachedResources';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const isLoadingComplete = useCachedResources();
+
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <View style={styles.container}>
+                <Profile />
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        //backgroundColor: '#fff',
+        //alignItems: 'center',
+        //justifyContent: 'center',
+    },
 });
+
+
+
+
