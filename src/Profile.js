e9f144499c16e1d1cb3e1c2767564cb44f1706e3
renatoAraujoSantos <50000041+renatoAraujoSantos@users.noreply.github.com>
2020-12-09 05:20:14 -0300

    teste

--- src/Profile.js
+++ src/Profile.js
@@ -69,7 +69,8 @@ export default function App() {
                         <View style={styles.activityIndicator}></View>
                         <View style={{ width: 250 }}>
                             <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
-                                Started following <Text style={{ fontWeight: "400" }}>Jake Challeahe</Text> and <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
+                                Started following <Text style={{ fontWeight: "400" }}>Jake Challeahe   
+                                </Text> and <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
                             </Text>
                         </View>
                     </View>
